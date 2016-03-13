e7b132a84e539b56b2fd0770255cb1ea73844e7a
Adam King <adam.king0126@gmail.com>
2016-03-13 11:36:26 -0500

    set up basic app, devise & oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#welcome'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
