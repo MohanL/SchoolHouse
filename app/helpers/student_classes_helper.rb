4dc8456f6d79abd3ddd7b8c211e1a4e1d7c5e3cc
Adam King <adam.king0126@gmail.com>
2016-09-24 15:57:14 -0500

    set default to attendance to "present" - a bit of a kludge

--- app/helpers/student_classes_helper.rb
+++ app/helpers/student_classes_helper.rb
@@ -4,4 +4,8 @@ module StudentClassesHelper
     Attendance.where(student_id: id, date: @date, status: status).length > 0 ? true : false
   end
 
+  def default_option?(id)
+    Attendance.where(student_id: id, date: @date).length == 0
+  end
+
 end
